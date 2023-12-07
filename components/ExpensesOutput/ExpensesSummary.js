1687745c07d5d4625fa5f8ddb08b0d963a4076c5
BillStephens2022 <two4onebill@yahoo.com>
2023-12-07 09:16:30 -0500

    added 3 new skeleton components for ExpensesOutput, ExpensesList, and ExpensesSummary

---
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from "react-native";
+
+function ExpensesSummary() {
+  return (
+    <View>
+      <Text>Last 7 Days</Text>
+      <Text>$518.43</Text>
+    </View>
+  );
+}
+
+export default ExpensesSummary;
+
+const styles = StyleSheet.create({});
